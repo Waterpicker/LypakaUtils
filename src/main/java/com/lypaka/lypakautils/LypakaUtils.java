ea010bd0913a02e0a652b4c43227324b566b35ea
BurstingF <Jensiwaa@gmail.com>
2022-06-17 01:21:04 +0200

    Update version

--- src/main/java/com/lypaka/lypakautils/LypakaUtils.java
+++ src/main/java/com/lypaka/lypakautils/LypakaUtils.java
@@ -17,7 +17,7 @@ public class LypakaUtils {
 
     public static final String MOD_ID = "lypakautils";
     public static final String MOD_NAME = "LypakaUtils";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.0.2";
     public static final Logger logger = LogManager.getLogger("LypakaUtils");
 
     @Mod.EventHandler
