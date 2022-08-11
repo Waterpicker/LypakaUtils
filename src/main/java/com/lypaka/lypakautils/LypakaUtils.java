a9388d7bfc4cdcbe6e46a33624399c2eae9109b5
Lypaka <ryan96t@gmail.com>
2022-08-11 11:29:14 -0500

    0.0.4 - WorldDimGetter

--- src/main/java/com/lypaka/lypakautils/LypakaUtils.java
+++ src/main/java/com/lypaka/lypakautils/LypakaUtils.java
@@ -18,7 +18,7 @@ public class LypakaUtils {
 
     public static final String MOD_ID = "lypakautils";
     public static final String MOD_NAME = "LypakaUtils";
-    public static final String VERSION = "0.0.2";
+    public static final String VERSION = "0.0.4";
     public static final Logger logger = LogManager.getLogger("LypakaUtils");
 
     @Mod.EventHandler
--- src/main/java/com/lypaka/lypakautils/WorldDimGetter.java
+++ src/main/java/com/lypaka/lypakautils/WorldDimGetter.java
@@ -1,15 +1,56 @@
 package com.lypaka.lypakautils;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+
+import java.util.Arrays;
 
 public class WorldDimGetter {
 
-    public static int getDimID (World world) {
+    public static int getDimID (World world, EntityPlayerMP player) {
 
         // I'm sorry to anyone who ever has to read what I'm about to do here.
-        String[] worldSplit = world.toString().split(", ");
-        String dim = worldSplit[1];
-        int dimensionID = Integer.parseInt(dim.replace("DimensionId=", ""));
+        int dimensionID = -99999; // default value because what are the chances of their being a dimension with this ID? Very slim I'd say
+        try {
+
+            // We try it the fast way first
+            String[] worldSplit = world.toString().split(", ");
+            String dim = worldSplit[1];
+            dimensionID = Integer.parseInt(dim.replace("DimensionId=", ""));
+
+        } catch (IndexOutOfBoundsException er) {
+
+            // But if that doesn't work then we unfortunately have to resort to this method
+            WorldServer[] worlds = world.getMinecraftServer().worlds;
+            for (WorldServer ws : worlds) {
+
+                if (ws.loadedEntityList.size() > 0) {
+
+                    for (Entity ent : ws.loadedEntityList) {
+
+                        if (ent instanceof EntityPlayerMP) {
+
+                            EntityPlayerMP p = (EntityPlayerMP) ent;
+                            if (p.getName().equalsIgnoreCase(player.getName())) {
+
+                                dimensionID = ws.provider.getDimension();
+                                break;
+
+                            }
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
         return dimensionID;
 
     }
